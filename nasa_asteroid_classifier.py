6f8b4a8d8d81d119d4cc8827f3bea216da396806
Kanishk <52484751+Kanishk598@users.noreply.github.com>
2020-01-24 17:36:38 +0530

    nasa asteroid classifier using K Nearest Neighbors

---
+++ nasa_asteroid_classifier.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+mydata = pd.read_csv("C:\\Users\\yvish\\Desktop\\nasa-asteroids-classification\\nasa.csv")
+#print(mydata.keys())
+
+#mydata.dtypes
+
+del mydata['Equinox']
+del mydata['Orbiting Body']
+del mydata['Orbit ID']
+del mydata['Neo Reference ID']
+del mydata['Name']
+
+mydata['Close Approach Date'] = pd.to_datetime(mydata['Close Approach Date'])
+mydata['Orbit Determination Date'] = pd.to_datetime(mydata['Orbit Determination Date'])
+
+mydata['Close Approach Date'] = pd.to_numeric(mydata['Close Approach Date'])
+mydata['Orbit Determination Date'] = pd.to_numeric(mydata['Orbit Determination Date'])
+
+x_train, x_test, y_train, y_test = train_test_split(mydata[['Absolute Magnitude', 'Est Dia in KM(min)', 'Est Dia in KM(max)','Est Dia in M(min)', 'Est Dia in M(max)', 'Est Dia in Miles(min)',
+       'Est Dia in Miles(max)', 'Est Dia in Feet(min)', 'Est Dia in Feet(max)',
+       'Close Approach Date', 'Epoch Date Close Approach',
+       'Relative Velocity km per sec', 'Relative Velocity km per hr',
+       'Miles per hour', 'Miss Dist.(Astronomical)', 'Miss Dist.(lunar)',
+       'Miss Dist.(kilometers)', 'Miss Dist.(miles)',
+       'Orbit Determination Date', 'Orbit Uncertainity',
+       'Minimum Orbit Intersection', 'Jupiter Tisserand Invariant',
+       'Epoch Osculation', 'Eccentricity', 'Semi Major Axis', 'Inclination',
+       'Asc Node Longitude', 'Orbital Period', 'Perihelion Distance',
+       'Perihelion Arg', 'Aphelion Dist', 'Perihelion Time', 'Mean Anomaly',
+       'Mean Motion']], mydata['Hazardous'], random_state = 0)
+
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(x_train, y_train)
+
